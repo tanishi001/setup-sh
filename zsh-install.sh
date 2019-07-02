6cc6180dcd15c8aec224d4fb6c7d4b2ce51d5ede
tanishi001 <e1n18048@oit.ac.jp>
2019-07-03 01:22:10 +0900

    git何もわからん

--- zsh-install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install -y zsh
-chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-zsh
-sed s/robbyrussell/candy/ ~/.zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-sed s/git/git zsh-syntax-highlighting zsh-completions/
