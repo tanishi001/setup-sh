19a36957ad20d60cf603471948272144eafaa215
tanishi001 <e1n18048@oit.ac.jp>
2019-07-03 02:15:29 +0900

    ディレクトリを直した

--- setup/vim-install.sh
+++ setup/vim-install.sh
@@ -6,7 +6,7 @@ cd ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
-cd ~/setup-sh
+cd ~/setup-sh/setup
 sh vim-dein.sh >> ~/.vimrc
 
 cat ~/.vimrc | sed -e "84a===call dein#add('tomasr/molokai')\n===call dein#add('Yggdroot/indentLine')\n===call dein#add('scrooloose/syntastic')\n===call dein#add('cohama/lexima.vim')\n===if has('lua')\n======call dein#add('Shougo/neocomplete.vim')\n======call dein#add('Shougo/neosnippet')\n======call dein#add('Shougo/neosnippet-snippets')\n===endif" | sed "s/===/  /" | sed "s/===/  /" > ~/.vimrc
