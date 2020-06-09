1e49a2a27a6260912c308e5a6f6803c7799d233c
tanishi001 <tanishi4423@gmail.com>
2020-06-10 02:28:48 +0900

    fix apps

--- manjaro/app/zsh.sh
+++ manjaro/app/zsh.sh
@@ -6,3 +6,6 @@ CHECK yay --needed -S zsh
 
 CHECK yay --needed -S powerline-rs
 
+CHECK git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+CHECK git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+
