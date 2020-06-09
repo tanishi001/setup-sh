816f8318615e566354a7b31ba98640860d30e1d4
tanishi001 <tanishi4423@gmail.com>
2020-06-10 01:54:01 +0900

    fix apps

--- manjaro/app/neovim.sh
+++ manjaro/app/neovim.sh
@@ -5,3 +5,6 @@ CHECK yay --needed -S neovim npm
 
 CHECK rm -rf $HOME/.config/nvim
 CHECK ln -sf $SETUP_DIR/resource/config/nvim $HOME/.config/nvim
+
+CHECK curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- manjaro/app/zsh.sh
+++ manjaro/app/zsh.sh
@@ -3,3 +3,6 @@ source $HOME/setup-sh/resource/import.sh
 
 CHECK yay --needed -S zsh
 [[ `echo $SHELL | grep 'zsh'` ]] || chsh -s /bin/zsh
+
+CHECK yay --needed -S powerline-rs
+
