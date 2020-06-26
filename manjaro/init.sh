89c1d477eadfc08c16816d84b5d2241f82ef2a25
tanishi001 <tanishi4423@gmail.com>
2020-06-26 18:45:50 +0900

    fix manjaro init.sh

--- manjaro/init.sh
+++ manjaro/init.sh
@@ -3,7 +3,7 @@ source $HOME/setup-sh/resource/import.sh
 
 CHECK sudo pacman -Syu
 CHECK sudo pacman -S git base-devel yay
-if [ -d ! $HOME/.config ]; then
+if [ ! -d $HOME/.config ]; then
     CHECK mkdir $HOME/.config
 fi
 
