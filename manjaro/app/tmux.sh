85c79ff5c5c2675b3771c29f685b3034381a2fce
tanishi001 <tanishi4423@gmail.com>
2020-07-07 02:21:49 +0900

    fix tmux.conf

--- manjaro/app/tmux.sh
+++ manjaro/app/tmux.sh
@@ -6,3 +6,4 @@ CHECK yay --needed -S tmux
 CHECK rm -rf $HOME/.tmux.conf
 CHECK ln -sf $SETUP_DIR/resource/dotfiles/.tmux.conf $HOME/.tmux.conf
 
+CHECK git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
