12ab9d822dd92420a8ceec134e855a2ddb54c053
tanishi001 <tanishi4423@gmail.com>
2020-07-07 01:39:34 +0900

    add tmux

---
+++ manjaro/app/tmux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source $HOME/setup-sh/resource/import.sh
+
+CHECK yay --needed -S tmux
+
+CHECK rm -rf $HOME/.tmux.conf
+CHECK ln -sf $SETUP_DIR/resource/dotfiles/.tmux.conf $HOME/.tmux.conf
+
