33811eecc822742e580c8950b4f374cd7ad4f3ee
tanishi001 <yuta.ta@icloud.com>
2021-05-18 17:24:44 +0900

    fix ubuntu neovim.sh

--- ubuntu/app/neovim.sh
+++ ubuntu/app/neovim.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source $HOME/setup-sh/resource/import.sh
 
-CHECK sudo apt install -y neovim nodejs
+CHECK sudo apt install -y neovim nodejs npm
+CHECK sudo npm install -g yarn
 
 CHECK rm -rf $HOME/.config/nvim
 CHECK ln -sf $SETUP_DIR/resource/config/nvim $HOME/.config/nvim
